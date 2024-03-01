fe4d2f3271ccd828fc35dd7908de82417ba1a51f
Cristian Ramirez <cristianramirezgt@gmail.com>
2024-02-29 23:53:05 -0600

    feat: Add json file as store for products

--- controllers/products.js
+++ controllers/products.js
@@ -14,20 +14,20 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  // products.push({ title: req.body.title });
   const product = new Product(req.body.title);
   product.save();
   res.redirect('/');
 };
 
 exports.getProducts =  (req, res, next) => {
-  const products = Product.fetchAll();
-  res.render('shop', {
-    products: products,
-    pageTitle: 'Shop',
-    path: '/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true 
+  Product.fetchAll(products => {
+    res.render('shop', {
+      products: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true 
+    });
   });
 };
--- models/product.js
+++ models/product.js
@@ -1,4 +1,6 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+const rootDir = require('../util/path');
 
 module.exports = class Product {
   constructor(title) {
@@ -6,11 +8,37 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    const file = path.join(
+      rootDir,
+      'data',
+      'products.json'
+    );
+
+    fs.readFile(file, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(file, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
+
   }
 
-  static fetchAll() {
-    return products;
+  static fetchAll(callback) {
+    const file = path.join(
+      rootDir,
+      'data',
+      'products.json'
+    );
+    fs.readFile(file, (err, fileContent) => {
+      if (err) {
+        callback([]);
+      }
+      callback(JSON.parse(fileContent));
+    });
   }
 
 }
