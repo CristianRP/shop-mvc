cfe93e62204762cc95e449ac63ba009bec5ed6c7
Cristian Ramirez <cristianramirezgt@gmail.com>
2024-03-11 11:47:28 -0600

    feat: Add pagination to index shop and add partial to include in other pages

--- controllers/shop.js
+++ controllers/shop.js
@@ -6,6 +6,8 @@ const PDFDocument = require('pdfkit');
 const Order = require('../models/order');
 const Product = require('../models/product');
 
+const ITEMS_PER_PAGE = 2;
+
 exports.getProducts =  (req, res, next) => {
   Product.find()
     .then(products => {
@@ -36,12 +38,27 @@ exports.getProduct = (req, res, next) => {
 }
 
 exports.getIndex = (req, res, next) => {
-  Product.find()
+  const page = +req.query.page || 1;
+  let totalItems;
+
+  Product.find().countDocuments()
+    .then(count => {
+      totalItems = count;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then(products => {
       res.render('shop/index', {
         products,
         pageTitle: 'Shop',
-        path: '/'
+        path: '/',
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
       });
     })
     .catch(err => {
